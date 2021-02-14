3c7306893d40dc203211ad4b6b6a1ff047de9e61
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2021-02-13 20:48:44 -0500

    Documentation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -18,6 +18,10 @@ export default () => {
       },
     });
 
+    // Think of this as an event listener being attached to the History object in the Container. Anytime the history object changes, onParentNavigate will be called
+    // The Container is setup to use the BrowserHistory and so the history object's path is the path in the address bar after the domain name. So, anytime that changes
+    // the event will fire and onParentNavigate will be called. This method is sent by the Marketing app as a return value to the mount method.
+    // This is an example of Microfrontend COMMUNICATING with the Container.
     history.listen(onParentNavigate);
   }, []);
 
